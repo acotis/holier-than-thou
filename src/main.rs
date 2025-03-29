ae62f1f012824e87b5c3afd490cb66c5cacd39d6
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2025-03-29 14:50:53 -0400

    Minor refactors.

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,17 @@ struct HoleLink {
     url: String,
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Solution {
+    bytes: u32,
+    chars: u32,
+    golfer: String,
+    hole: String,
+    lang: String,
+    scoring: String,
+    submitted: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
 
@@ -34,34 +45,33 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     // Collect the full solutions log for each hole, in Rust.
 
-    let futures = holes.iter().map(|hole| get_solution_log(&hole.id));
+    let futures = holes.iter().map(|hole| (async || (hole.id.clone(), get_solution_log(&hole.id).await))());
 
     println!("Fetching API data...");
     let responses = futures_util::future::join_all(futures).await;
     println!("Done.");
 
     for response in responses {
-        println!("{:35} {}", response.0, response.1);
+        println!("{:35} {:?}", response.0, response.1);
     }
 
     Ok(())
 }
 
-async fn get_solution_log(hole_id: &str) -> (String, String) {
+async fn get_solution_log(hole_id: &str) -> Vec<Solution> {
     let url = format!(
         "http://code.golf/api/solutions-log?hole={}&lang=rust",
         urlencoding::encode(hole_id),
     );
 
-    for _attempt in 0..3 {
+    for _attempt in 0..10 {
         let resp = reqwest::get(&url).await.unwrap();
         if !resp.status().is_success() {continue;}
         let text = resp.text().await.unwrap();
 
-        let snip = text[..text.len().min(20)].to_owned();
-        return (hole_id.to_owned(), snip);
+        return serde_json::from_str(&text).expect("could not parse solution log");
     }
 
-    panic!("When fetching solutions log for hole \"{hole_id}\", the code.golf API gave a non-2XX status code for three attempts in a row. The code.golf API is a little unstable, so you might just try re-running the script.");
+    panic!("When fetching solutions log for hole \"{hole_id}\", the code.golf API gave a non-2XX status code for 10 attempts in a row. The code.golf API is a little unstable, so you might just try re-running the script.");
 }
 
